6949d94c3831d02a7761f2e8b59010a0c4ba228b
Patrique Cervera <patrique.cervera@gmail.com>
2022-07-09 00:25:41 +0800

    update view

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class EmployeeController extends Controller
 {
     public function index () {
-        return view('employee');
+        return view('employeedashboard');
     }
 
     public function payroll() {
